157789a6c42d985c65bd54ee60360b8d5755d1c0
taka205 <william_corbari@outlook.com.br>
2025-04-15 21:46:16 -0300

    Add files via upload

---
+++ william.py
@@ -0,0 +1,8 @@
+nome_digitado = input("digite seu nome")
+print("ola,", nome_digitado + "!")
+print("como esta seu dia?")
+resposta = ("bem")
+resposta2 = ("mais o menos")
+resposta3 = ("mal")
+resposta = print("que bom,", nome_digitado + "!")
+resposta2 = print("o que aconteceu,", nome_digitado + "?")
